2d8dec1881f9b67191d4bab88d319fa674523289
ashiqee <ashiq.buet73@gmail.com>
2024-07-09 23:17:46 +0600

    Banner typescript

--- src/components/reusableComponents/PageBanner.tsx
+++ src/components/reusableComponents/PageBanner.tsx
@@ -7,7 +7,12 @@ import {
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
 
-const PageBanner = ({ bannerProps,img }) => {
+interface TPageBannerProps {
+    bannerProps: string[];
+    img: string;
+}
+
+const PageBanner = ({ bannerProps,img }:TPageBannerProps) => {
   return (
     <div>
       <Breadcrumb className="min-h-[160px] md:min-h-[240px] 2xl:h-[360px] flex justify-center uppercase bg-cover object-center items-center"
@@ -15,15 +20,15 @@ const PageBanner = ({ bannerProps,img }) => {
       >
         <BreadcrumbList className='2xl:pt-14 pt-0'>
           <BreadcrumbItem>
-            <BreadcrumbLink href={bannerProps >= 3 ? `/${bannerProps[1]}`:"/"}>
-            {bannerProps >= 3 ? bannerProps[1]:"Home"}
+            <BreadcrumbLink href={bannerProps?.length >= 3 ? `/${bannerProps[1]}`:"/"}>
+            {bannerProps?.length >= 3 ? bannerProps[1]:"Home"}
            
             
             </BreadcrumbLink>
           </BreadcrumbItem>
           <BreadcrumbSeparator />
           <BreadcrumbItem>
-            <BreadcrumbPage>{bannerProps >= 3 ? bannerProps[2]:bannerProps[1]}</BreadcrumbPage>
+            <BreadcrumbPage>{bannerProps?.length >= 3 ? bannerProps[2]:bannerProps[1]}</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
