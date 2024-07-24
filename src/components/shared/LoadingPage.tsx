843e00c7b53995d35b0bad1fd5d8e0fb0ff13c23
ashiqee <ashiq.buet73@gmail.com>
2024-07-24 19:08:01 +0600

    vercel live link responsive issue solved

--- src/components/shared/LoadingPage.tsx
+++ src/components/shared/LoadingPage.tsx
@@ -8,7 +8,7 @@ const LoadingPage = () => {
                 <div className="flex  justify-center  items-center">
                     
                     <img className="w-[20rem] relative md:w-full" src="https://i.postimg.cc/k4q8dSTY/X7-Zcr8yp7-B6issio-Ep.webp" alt="" />
-                    <div className="animate-pulse absolute md:right-96 md:top-24 top-80 right-14">
+                    <div className="animate-pulse absolute md:right-96 md:top-24 2xl:top-60 2xl:right-[700px] top-80 right-14">
                     <Logo/>
                     </div>
                 </div>
